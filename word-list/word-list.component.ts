ad8e4f57c1bf37047416d62a22fb1be3779a10bf
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2021-04-21 17:57:35 +0200

    Edit words saved.

---
+++ word-list/word-list.component.ts
@@ -0,0 +1,67 @@
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {WordsService} from '../services/words.service';
+import {WordFire} from '../models/firestore/wordFire';
+import {ActivatedRoute} from '@angular/router';
+import {EditWordDialogComponent} from '../dialogs/edit-word-dialog/edit-word-dialog.component';
+import {EditableWord} from '../models/yaml/wordYML';
+import {MatDialog} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-word-list',
+  templateUrl: './word-list.component.html',
+  styleUrls: ['./word-list.component.css']
+})
+export class WordListComponent implements OnInit, OnDestroy{
+
+  dictionaryId: string;
+
+  words: WordFire[] = [];
+  private sub: any;
+
+  constructor(private wordsService: WordsService,
+              private route: ActivatedRoute,
+              public dialog: MatDialog) { }
+
+ edit(wordIndex: number) {
+    const word = this.words[wordIndex];
+    const dialogRef = this.dialog.open(EditWordDialogComponent, {
+      data: {
+        word,
+        documentInDictionaryNum: undefined,
+        wordIndex
+      },
+      width: '50%',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // console.log('RESULT: ' + typeof(result));
+      if (result !== undefined && result) {
+        // console.log(result);
+        const eword = result as EditableWord;
+        // this.words[wordIndex] = eword.word;
+      }
+    });
+  }
+
+  delete(wordIndex: number) {}
+
+
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+       this.dictionaryId = params.id; // (+) converts string 'id' to a number
+
+       this.wordsService.getWords(this.dictionaryId).subscribe((data) => {
+        for (const doc of data) {
+          const word = doc.payload.doc.data() as WordFire;
+          this.words.push(word);
+          // console.log(word);
+        }
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+}
